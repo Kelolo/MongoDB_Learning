f9f9b9e2997bfef416014e1e8ca0918b4735581d
Allen Zhao <cz264@njit.edu>
2017-03-28 10:38:25 -0400

    added MongoDBListener class

--- src/com/userdbmanagement/mongodb/listener/MongoDBListener.java
+++ src/com/userdbmanagement/mongodb/listener/MongoDBListener.java
@@ -1,5 +1,22 @@
 package com.userdbmanagement.mongodb.listener;
 
-public class MongoDBListener {
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
+public class MongoDBListener implements ServletContextListener{
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
